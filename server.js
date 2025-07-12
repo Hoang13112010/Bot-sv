d19add8e2ff5a5c25797a29cf5f93702b327a939
Hoang13112010 <102331822+Hoang13112010@users.noreply.github.com>
2025-07-12 20:54:58 +0700

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Minecraft bot is running!'));
-app.listen(3000, () => console.log('Web server chạy tại cổng 3000'));
